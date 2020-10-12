764a8fa4e17f9299384ab35eada110e813faeca1
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-13 05:55:41 +0700

    #9 - Add KNote Properties

---
+++ src/main/java/com/hendisantika/knote/config/KnoteProperties.java
@@ -0,0 +1,34 @@
+package com.hendisantika.knote.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : knote
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 13/10/20
+ * Time: 05.54
+ */
+@ConfigurationProperties(prefix = "knote")
+public class KnoteProperties {
+    @Value("${minio.host:localhost}")
+    private String minioHost;
+
+    @Value("${minio.bucket:image-storage}")
+    private String minioBucket;
+
+    @Value("${minio.access.key:}")
+    private String minioAccessKey;
+
+    @Value("${minio.secret.key:}")
+    private String minioSecretKey;
+
+    @Value("${minio.useSSL:false}")
+    private boolean minioUseSSL;
+
+    @Value("${minio.reconnect.enabled:true}")
+    private boolean minioReconnectEnabled;
+}
