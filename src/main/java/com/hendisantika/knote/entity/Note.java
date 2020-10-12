64af35d978292643eeb8225275a1fa4d92fff6c7
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-13 05:53:17 +0700

    #7 - Add Note Entity

---
+++ src/main/java/com/hendisantika/knote/entity/Note.java
@@ -0,0 +1,33 @@
+package com.hendisantika.knote.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : knote
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 13/10/20
+ * Time: 05.52
+ */
+@Document(collection = "notes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+class Note {
+    @Id
+    private String id;
+    private String description;
+
+    @Override
+    public String toString() {
+        return description;
+    }
+}
