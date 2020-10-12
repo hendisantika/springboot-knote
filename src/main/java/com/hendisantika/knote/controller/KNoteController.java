bd567d4f809a0916e829047effd2a472f668af98
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-13 06:09:04 +0700

    #12 - Add saveNotes API  in KNote Controller

--- src/main/java/com/hendisantika/knote/controller/KNoteController.java
+++ src/main/java/com/hendisantika/knote/controller/KNoteController.java
@@ -4,6 +4,7 @@ import com.hendisantika.knote.config.KnoteProperties;
 import com.hendisantika.knote.entity.Note;
 import com.hendisantika.knote.repository.NotesRepository;
 import io.minio.MinioClient;
+import org.commonmark.node.Node;
 import org.commonmark.parser.Parser;
 import org.commonmark.renderer.html.HtmlRenderer;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,9 +12,14 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.PostConstruct;
 import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by IntelliJ IDEA.
@@ -82,6 +88,47 @@ public class KNoteController {
         List<Note> notes = notesRepository.findAll();
         Collections.reverse(notes);
         model.addAttribute("notes", notes);
+    }
+
+    @PostMapping("/note")
+    public String saveNotes(@RequestParam("image") MultipartFile file,
+                            @RequestParam String description,
+                            @RequestParam(required = false) String publish,
+                            @RequestParam(required = false) String upload,
+                            Model model) throws Exception {
+
+        if (publish != null && publish.equals("Publish")) {
+            saveNote(description, model);
+            getAllNotes(model);
+            return "redirect:/";
+        }
+        if (upload != null && upload.equals("Upload")) {
+            if (file != null && file.getOriginalFilename() != null && !file.getOriginalFilename().isEmpty()) {
+                uploadImage(file, description, model);
+            }
+            getAllNotes(model);
+            return "index";
+        }
+        return "index";
+    }
+
+    private void uploadImage(MultipartFile file, String description, Model model) throws Exception {
+        String fileId = UUID.randomUUID().toString() + "." + file.getOriginalFilename().split("\\.")[1];
+        minioClient.putObject(properties.getMinioBucket(), fileId, file.getInputStream(),
+                file.getSize(), null, null, file.getContentType());
+        model.addAttribute("description",
+                description + " ![](/img/" + fileId + ")");
+    }
+
+    private void saveNote(String description, Model model) {
+        if (description != null && !description.trim().isEmpty()) {
+            //You need to translate markup to HTML
+            Node document = parser.parse(description.trim());
+            String html = renderer.render(document);
+            notesRepository.save(new Note(null, html));
+            //After publish you need to clean up the textarea
+            model.addAttribute("description", "");
+        }
     }
 
 }
