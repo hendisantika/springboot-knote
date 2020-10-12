2f12cd774b93b63b75b5a55aaa13f2d6c346673e
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-13 05:44:27 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/knote/KnoteApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.knote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/knote/KnoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.knote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
